0fd9bac00ec1c5e70372065fc8f1af10b8a00a0a
arslan0044 <arslan.saqib121@gmail.com>
2024-04-17 12:29:04 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 // import { BRANDSVGAnimation } from "./components/AnimatedSVG";
+import { motion } from "framer-motion";
 import Header from "./components/Header";
 // import {BERLINSVGAnimation,TAILORMADESVGAnimation,NEWSVGTest} from "./components/AnimatedSVG"
-
+// motion
 import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
+import Section3 from "./Section3";
 function App() {
   return (
     <div div className=" overflow-hidden">
       {/* <NEWSVGTest/> */}
       <Header />
-     <Section1/>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    
+      
+
+
+
+
+
+
+      
     </div>
   );
 }
