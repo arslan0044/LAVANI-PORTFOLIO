374337f1d01d08c39ea5fd115ed357ea3acf7de1
arslan0044 <arslan.saqib121@gmail.com>
2024-04-19 19:10:34 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Section9 from "./components/Section9";
 import Section10 from "./components/Section10";
 import Section11 from "./components/Section11";
 import Section12 from "./components/Section12";
+import Section13 from "./components/Section13";
 function App() {
   return (
     <div div className=" overflow-hidden">
@@ -32,6 +33,7 @@ function App() {
       <Section10/>
       <Section11/>
       <Section12/>
+      <Section13/>
 
       {/*     
       <TAILORMADESVGAnimation/>
